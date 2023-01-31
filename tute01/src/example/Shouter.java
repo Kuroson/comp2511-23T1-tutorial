2c6cf4b08a88c356e9d9435e1258a8ea6fc47d2d
Alvin <39092498+Kuroson@users.noreply.github.com>
2023-01-31 13:48:27 +1100

    Initial commit

---
+++ tute01/src/example/Shouter.java
@@ -0,0 +1,25 @@
+package example;
+
+/**
+ * Write a program that stores a message and has methods for getting the message,
+ * updating the message and printing it out in all caps. Write a `main()` method
+ * for testing this class.
+ *
+ * Create a:
+ * - Getter
+ * - Setter
+ * - Constructor
+ * - printMe
+ * - shout
+ * - toString
+ */
+public class Shouter {
+    public static void main(String[] args) {
+        // Shouter s = new Shouter("This is my message");
+        // s.printMe();
+        // s.shout();
+        // When printing objects, Java will try and stringify
+        // In this case, it calls the .toString() method
+        // System.out.println(s);
+    }
+}

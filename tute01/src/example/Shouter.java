9f08c977c8a3d48813b47ef845d22a416541cbff
Alvin Cherk <a.cherk@unsw.edu.au>
2023-02-15 22:32:13 +1100

    Add some more annotations

--- tute01/src/example/Shouter.java
+++ tute01/src/example/Shouter.java
@@ -14,15 +14,27 @@ package example;
  * - toString
  */
 public class Shouter {
+    // `private` is an access identifier. `public` is another access identifier
+    // Generally we set ALL attributes to private for
+    // security and data encapsulation (explored next week by the course)
     private String message;
 
     public Shouter(String message) {
+        // Here, we have to use `this.message` to refer to the class's version of the message variable
+        // This is because there are two conflicting names in the same scope.
+        // 1. Parameter of constructor
+        // 2. Class attribute/variable itself
         this.message = message;
+        //              ^ refers to the value passed into the constructor
+        // ^ Refers to the class's attribute message
     }
 
     public String getMessage() {
         // NOTE: You don't have to use the keyword `this`
         // But I use it because of clarity
+        // I generally use `this.` as it makes it very clear
+        // Where the variable is coming from (from the class itself)
+        // Thereby, any other variable can be assumed to belong to the scope of the function/codeblock
         return this.message;
     }
 
@@ -56,5 +68,7 @@ public class Shouter {
         // When printing objects, Java will try and stringify
         // In this case, it calls the .toString() method
         System.out.println(s);
+        // Equivalent is
+        // System.out.println(s.toString());
     }
 }

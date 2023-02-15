da1dbe33da7b3d15f35c3e735c61d15cb8a639cd
Alvin Cherk <a.cherk@unsw.edu.au>
2023-02-15 15:11:49 +1100

    Add code week 1

--- tute01/src/example/HelloWorld.java
+++ tute01/src/example/HelloWorld.java
@@ -4,11 +4,11 @@ package example;
  * Prints "Hello World" to the console.
  *
  * @author Robert Clifton-Everest
- *
  */
 public class HelloWorld {
     public static void main(String[] args) {
         // TODO Complete this method
-
+        System.out.printf("%s\n", "Hello World");
+        System.out.println("Hello World");
     }
 }
--- tute01/src/example/Sum.java
+++ tute01/src/example/Sum.java
@@ -1,10 +1,22 @@
 package example;
 
+import java.util.Scanner;
+
 /**
  * Write a program that uses the `Scanner` class
  * which reads in a line of numbers separated by spaces,
  * and sums them.
  */
 public class Sum {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] numbers = input.split(" ");
+        int sums = 0;
+        for (String x : numbers) {
+            sums += Integer.parseInt(x);
+        }
+        System.out.println(sums);
+        scanner.close();
+    }
 }
